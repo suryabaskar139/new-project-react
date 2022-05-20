1d83da01c01960b3a13c3dc13f7997b58ab3dee4
suryabaskar139 <suryabaskar139@gmail.com>
2022-05-20 23:05:05 +0530

    master

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Services from './components/Services';
+import Pricing from './components/Pricing';
+import Rooms from './components/Rooms';
+import Footer from './components/Footer';
+
+
+
+function App() {
+  return (
+    <>
+
+    <Navbar />
+    <Header />
+    <Services />
+    <Pricing />
+    <Rooms />
+    <Footer />
+
+  
+    
+
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+ <section id="home">
+    <div class="container text-center">
+       <div class="row justify-content-center">
+          <div class="col-md-10">
+             <h1 class="text-white display-4">WE ARE CREATIVE AGENCY, CREATIVE PEOPLE</h1>
+             <p class="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-les</p>
+             <a href="#contact" class="learn-more-btn">LOG IN</a>
+             <a href="#contact" class="mx-2 learn-more-btn">SIGN UP</a>
+          </div>
+       </div>
+    </div>
+ </section>
+
+  )
+}
+
+export default Header
